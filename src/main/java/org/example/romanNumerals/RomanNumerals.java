a901bdb2e193aeb2292b588c19b40dab300279c1
TTurvey <tturvey10@gmail.com>
2023-04-20 16:52:41 +0100

    Roman Numerals - refactor for enum

--- src/main/java/org/example/romanNumerals/RomanNumerals.java
+++ src/main/java/org/example/romanNumerals/RomanNumerals.java
@@ -3,60 +3,41 @@ package org.example.romanNumerals;
 public class RomanNumerals {
 
 
-    public String translate(int i) {
+    public String translate(int input) {
         String roman = "";
-        while (i >= 1000) {
-            roman += "M";
-            i -= 1000;
-        }
-        if (i == 900) {
-            roman += "CM";
-            i -= 900;
-        }
-        while (i >= 500) {
-            roman += "D";
-            i -= 500;
-        }
-        if (i == 400) {
-            roman += "CD";
-            i -= 400;
-        }
-        while (i >= 100) {
-            roman += "C";
-            i -= 100;
-        }
-        if (i == 90) {
-            roman += "XC";
-            i -= 90;
-        }
-        if (i >= 50) {
-            roman += "L";
-            i -= 50;
-        }
-        if (i == 40) {
-            roman += "XL";
-            i -= 40;
-        }
-        while (i >= 10) {
-            roman += "X";
-            i -= 10;
-        }
-        if (i == 9) {
-            roman += "IX";
-            i -= 9;
-        }
-        if (i >= 5) {
-            roman += "V";
-            i -= 5;
-        }
-        if (i == 4) {
-            roman += "IV";
-            i -= 4;
-        }
-        while (i > 0) {
-            roman += "I";
-            i -= 1;
+        for (ArabicToRoman arabicToRoman : ArabicToRoman.values()) {
+            while (input >= arabicToRoman.arabic) {
+                roman += arabicToRoman.roman;
+                input -= arabicToRoman.arabic;
+            }
         }
         return roman;
     }
+
+    enum ArabicToRoman {
+        THOUSAND(1000, "M"),
+        NINE_HUNDRED(900, "CM"),
+        FIVE_HUNDRED(500, "D"),
+        FOUR_HUNDRED(400, "CD"),
+        HUNDRED(100, "C"),
+        NINETY(90, "XC"),
+        FIFTY(50, "L"),
+        FORTY(40, "XL"),
+        TEN(10, "X"),
+        NINE(9, "IX"),
+        FIVE(5, "V"),
+        FOUR(4, "IV"),
+        ONE(1, "I");
+
+        private final int arabic;
+        private final String roman;
+
+        ArabicToRoman(int arabic, String roman) {
+            this.arabic = arabic;
+            this.roman = roman;
+        }
+    }
 }
+
+
+
