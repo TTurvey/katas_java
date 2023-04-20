bdf999ef1002f589966afe9ded67a29b35284cbb
TTurvey <tturvey10@gmail.com>
2023-04-20 17:19:03 +0100

    LeapYearChecker - returns false if year is divisible by 100 but not 400

--- src/main/java/org/example/leapYear/LeapYearChecker.java
+++ src/main/java/org/example/leapYear/LeapYearChecker.java
@@ -2,13 +2,7 @@ package org.example.leapYear;
 
 public class LeapYearChecker {
 
-    public boolean evaluate(int i) {
-        if (i % 4 == 0) {
-            return true;
-        }
-        if (i % 400 == 0) {
-            return true;
-        }
-        return false;
+    public boolean evaluate(int year) {
+        return year % 4 == 0 && !(year % 100 == 0 && year % 400 != 0);
     }
 }
--- src/test/java/org/example/leapYear/leapYearCheckerTest.java
+++ src/test/java/org/example/leapYear/leapYearCheckerTest.java
@@ -10,21 +10,28 @@ class leapYearCheckerTest {
     public void
     returns_false_if_year_is_not_divisible_by_4() {
         LeapYearChecker leapYearChecker = new LeapYearChecker();
-        assertFalse(leapYearChecker.evaluate(3));
+        assertFalse(leapYearChecker.evaluate(1997));
     }
 
     @Test
     public void
     returns_true_if_year_is_divisible_by_4() {
         LeapYearChecker leapYearChecker = new LeapYearChecker();
-        assertTrue(leapYearChecker.evaluate(4));
+        assertTrue(leapYearChecker.evaluate(1996));
     }
 
     @Test
     public void
     returns_true_if_year_is_divisible_by_400() {
         LeapYearChecker leapYearChecker = new LeapYearChecker();
-        assertTrue(leapYearChecker.evaluate(400));
+        assertTrue(leapYearChecker.evaluate(1600));
+    }
+
+    @Test
+    public void
+    returns_false_if_year_is_divisible_by_100_but_not_by_400() {
+        LeapYearChecker leapYearChecker = new LeapYearChecker();
+        assertFalse(leapYearChecker.evaluate(1800));
     }
 
 }
