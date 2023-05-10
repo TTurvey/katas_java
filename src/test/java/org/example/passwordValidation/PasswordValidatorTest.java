bda714294a1c208fc502296f676ac90248c9ed0e
TTurvey <tturvey10@gmail.com>
2023-05-10 16:11:23 +0100

    Password Validation - returns true is input has more than 8 characters

--- src/test/java/org/example/passwordValidation/PasswordValidatorTest.java
+++ src/test/java/org/example/passwordValidation/PasswordValidatorTest.java
@@ -14,4 +14,12 @@ class PasswordValidatorTest {
         assertTrue(pv.check("abcdefgh"));
     }
 
+    @Test
+    public void
+    returns_true_if_input_has_more_than_8_characters() {
+        PasswordValidator pv = new PasswordValidator();
+
+        assertTrue(pv.check("abcdefghi"));
+    }
+
 }
